58342940c53ed3773c51ff1b2189dd0a6b52fcb7
Brady Bellini <bbellini@pm.me>
2022-04-05 00:04:04 -0700

    added tailwindcss

---
+++ v7/src/routes/__layout.svelte
@@ -0,0 +1,20 @@
+<script>
+    import '../styles/app.css';
+</script>
+
+<header>
+   
+</header>
+
+<main>
+    <slot></slot>
+</main>
+
+<footer>
+    
+</footer>
+
+
+<style>
+
+</style>
--- v7/src/routes/index.svelte
+++ v7/src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<div class="grid grid-cols-4 gap-4">
+    <div class="bg-slate-500">01</div>
+    <div class="bg-slate-500">01</div>
+    <div class="bg-slate-500">01</div>
+    <div class="bg-slate-500">01</div>
+    <div class="bg-slate-500">01</div>
+    <div class="bg-slate-500">01</div>
+  </div>
