e7577166cf96152cced384b66de732c61952a6e2
Brady Bellini <bbellini@pm.me>
2022-06-07 23:39:34 -0700

    Update contact.svelte

--- v7/src/routes/contact.svelte
+++ v7/src/routes/contact.svelte
@@ -1,4 +1,3 @@
 <p>
-	You can contact me by emailing me at first initial, last name (name is at the top of the website
-	if you forgot) at proton.me. I look forward to hearing from you!
+	You can contact me by emailing me: website_at_bradybellini.com. I look forward to hearing from you!
 </p>
