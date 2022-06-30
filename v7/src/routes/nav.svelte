3f0b53770216afb2051f2adbd92cc1fc3a7cbaf9
Brady Bellini <bbellini@pm.me>
2022-06-30 16:51:27 -0700

    Update nav.svelte

--- v7/src/routes/nav.svelte
+++ v7/src/routes/nav.svelte
@@ -1,7 +1,7 @@
 <p>Random links to stuff on my personal site or related</p>
 <br />
 <ul>
-	<li><a href="https://technonomicon.io/" class="hover:text-white underline">My Tech and privacy newsletter</a></li>
+	<li><a href="https://technonomicon.io/" class="hover:text-white underline">My tech and privacy newsletter</a></li>
 	<li><a href="https://bbellini.io/" class="hover:text-white underline">Personal Startpage</a></li>
 	<li><a href="/domains" class="hover:text-white underline">Domains I Own</a></li>
 </ul>
