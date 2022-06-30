bd21faf5fa3811ca1f18748d16db2a794cc15540
Brady Bellini <bbellini@pm.me>
2022-06-30 16:48:23 -0700

    Update index.svelte

--- v7/src/routes/index.svelte
+++ v7/src/routes/index.svelte
@@ -3,3 +3,4 @@
 	currently looking for new opportunities. I enjoy working with interesting data and solving
 	problems with awesome tech.
 </p>
+<li><a href="https://technonomicon.io/" class="hover:text-white underline">Subscribe to my newsletter about tech, privacy, and the open web</a></li>
