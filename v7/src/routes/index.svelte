0b7fe9b7f8675d1c427c14e1ec5db01616096de2
Brady Bellini <bbellini@pm.me>
2022-11-28 17:53:40 -0800

    Update index.svelte

--- v7/src/routes/index.svelte
+++ v7/src/routes/index.svelte
@@ -1,6 +1,5 @@
 <p>
-	Hey, I'm Brady, a Business Intelligence Analyst with 3+ years of data analyst experience,
-	currently looking for new opportunities. I enjoy working with interesting data and solving
+	Hey, I'm Brady, a Business Intelligence Analyst with 3+ years of data analyst experience. I enjoy working with interesting data and solving
 	problems with awesome tech.
 </p>
 <br>
